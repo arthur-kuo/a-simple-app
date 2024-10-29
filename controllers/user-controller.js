463674ba25aaab384836bb2706fe9d0c974ea92c
arthur.kuo <arthur.kuo@infotimes.com.tw>
2024-10-29 17:53:28 +0800

    feat:add user signup

---
+++ controllers/user-controller.js
@@ -0,0 +1,46 @@
+const {User} = require('../models');
+const {sendVerificationEmail} = require('../utils/emailService');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const signUp = async (req, res) => {
+  try {
+    const {email, password, confirmPassword, name} = req.body;
+
+    // Check if the password and confirmPassword match
+    if (password !== confirmPassword) {
+      return res.status(400).send('Passwords do not match');
+    }
+
+    // Password validation regex
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passwordRegex.test(password)) {
+      return res.status(400).send(
+        'Invalid password format. Password must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long.'
+      );
+    }
+
+    const user = await User.create({email, password, name});
+    await sendVerificationEmail(user); // use SendGrid
+    res.status(201).send('Registration successful, please verify your email');
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+
+const login = async (req, res) => {
+
+};
+
+const googleAuth = async (req, res) => {
+  // Handle Google OAuth callback
+};
+
+const emailVerification = async (req, res) => {
+  // Use SendGrid to send verification emails
+};
+
+module.exports = {signUp, login, googleAuth, emailVerification};
---
+++ routes/modules/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const {
+  signUp,
+  login,
+  googleOAuthCallback,
+} = require('../controllers/user-controller');
+
+router.post('/signup', signUp);
+router.post('/login', login);
+router.get('/google/callback', googleOAuthCallback);
+
+module.exports = router;
