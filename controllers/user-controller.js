2c3fbffa5f79f75e6bcc2d062ac40483ac5ec3df
arthur.kuo <arthur.kuo@infotimes.com.tw>
2024-10-31 18:32:25 +0800

    feat: refactoring user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const signUp = async (req, res) => {
+const signUp = async (req, res, next) => {
   try {
     const {name, email, password, confirmPassword} = req.body;
 
@@ -50,9 +50,8 @@ const signUp = async (req, res) => {
     res.status(201).json({
       message: 'Registration successful. Please verify your email.',
     });
-  } catch (error) {
-    console.error('Error during sign up:', error.message);
-    res.status(500).json({error: 'Internal server error.'});
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -92,21 +91,25 @@ const logout = (req, res, next) => {
   }
 };
 
-const emailVerification = async (req, res) => {
+const emailVerification = async (req, res, next) => {
   try {
     const {token} = req.query;
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findByPk(decoded.userId);
 
-    if (!user) return res.status(404).send('User not found');
-    if (user.isVerified) return res.status(400).send('User already verified');
+    if (!user) {
+      return res.status(404).json({error: 'User not found'});
+    };
+    if (user.isVerified) {
+      return res.status(400).json({error: 'User already verified'});
+    };
 
     user.isVerified = true;
     await user.save();
 
-    res.status(200).send('Email verification successful');
-  } catch (error) {
-    res.status(400).send('Invalid or expired token');
+    return res.status(200).json({message: 'Email verification successful'});
+  } catch (err) {
+    next(err);
   }
 };
 
