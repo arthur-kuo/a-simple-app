a47cfd976267fcff2c18484a2cddfeda4e93cc6a
arthur.kuo <arthur.kuo@infotimes.com.tw>
2024-11-04 10:22:55 +0800

    feat: import Op to admin controller

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,7 @@
 const {User} = require('../models');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const {Op} = require('sequelize');
 
 const login = async (req, res, next) => {
   try {
