f367f9fc96312d2096d4f36b0357097a2a1d4458
arthur.kuo <arthur.kuo@infotimes.com.tw>
2024-11-04 17:17:29 +0800

    feat: fix user router

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -14,10 +14,10 @@ const {
 router.post('/signup', signUp);
 router.post('/login', login);
 router.post('/logout', logout);
+router.get('/dashboard', getUsers);
+router.get('/dashboard/stats', getUsersStatistics);
 router.put('/:id/reset-password', editUserPassword);
 router.get('/:id', getUserInfo);
 router.put('/:id', editUserInfo);
-router.get('/dashboard', getUsers);
-router.get('/dashboard/stats', getUsersStatistics);
 
 module.exports = router;
