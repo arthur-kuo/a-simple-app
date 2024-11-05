d3977b220ec46f5ade5011ecb354010131efa529
arthur.kuo <arthur.kuo@infotimes.com.tw>
2024-11-05 11:07:49 +0800

    feat: fix auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,16 @@
-const {ensureAuthenticated, getUser} = require('../helpers/auth-helpers');
+const {ensureAuthenticated} = require('../helpers/auth-helpers');
+const passport = require('../config/passport')
+
 const authenticated = (req, res, next) => {
-  // if (req.isAuthenticated)
-  if (ensureAuthenticated(req)) {
-    return next();
-  }
-  res.redirect('/api/users/login');
-};
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) {
+      return res.status(401).json({ status: 'error', message: 'Unauthorized' })
+    }
+    req.user = user // JWT 沒有使用 session，所以需要手動設置
+    return next()
+  })(req, res, next)
+}
+
 module.exports = {
   authenticated,
 };
