917a727f411ae7cb906ca46bc8361c283b1b5ff9
arthur.kuo <arthur.kuo@infotimes.com.tw>
2024-11-07 12:00:54 +0800

    feat: fix email verifaction

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -120,6 +120,8 @@ const emailVerification = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findByPk(decoded.userId);
 
+    console.log(decoded)
+
     if (!user) {
       return res.status(404).json({error: 'User not found'});
     };
